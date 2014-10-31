0b2d0b7a5ca62b685810e2094732cd3696fe5095
mknblch <mknblch@gmail.com>
2014-10-31 09:17:08 +0100

    refact

--- src/test/java/de/mknblch/sucode/inspection/InspectorTest.java
+++ src/test/java/de/mknblch/sucode/inspection/InspectorTest.java
@@ -16,54 +16,45 @@ import static org.junit.Assert.*;
 public class InspectorTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(InspectorTest.class);
-
     private static final Parser PARSER = new Parser();
 
     @Test
     public void testInspect() throws Exception {
-
-        final Program parse = PARSER.parse("(1 (2 nil) 3 ) 5 (6(7)) 8 9");
+        final Program program = PARSER.parse("(1 (2 nil) 3 ) 5 (6(7)) 8 9");
 
         final InspectionRule replaceRule = new InspectionRule() {
-
             private int c = 0;
-
             @Override
             public void inspect(ListStruct listElement) {
-
                 final Object car = listElement.car();
                 if (null == car) {
                     listElement.setCar(new ListStruct(++c));
                 } else {
                     final Integer o = (Integer) listElement.car();
-                    c += o;
-                    listElement.setCar(new ListStruct(c));
+                    listElement.setCar(new ListStruct(c += o));
                 }
             }
-
             @Override
             public boolean followSublist(ListStruct list) {
                 return true;
             }
         };
 
-        LOGGER.debug("{}", FormatHelper.formatPretty(parse));
-        Inspector.inspect(parse, replaceRule);
-        LOGGER.debug("{}", FormatHelper.formatPretty(parse));
+        LOGGER.debug("{}", FormatHelper.formatPretty(program));
+        Inspector.inspect(program, replaceRule);
+        LOGGER.debug("{}", FormatHelper.formatPretty(program));
 
-        assertASTEquals("( ( ( 1 ) ( ( 3 ) ( 4 ) ) ( 7 ) ) ( 12 ) ( ( 18 ) ( ( 25 ) ) ) ( 33 ) ( 42 ) )", parse);
+        assertASTEquals("( ( ( 1 ) ( ( 3 ) ( 4 ) ) ( 7 ) ) ( 12 ) ( ( 18 ) ( ( 25 ) ) ) ( 33 ) ( 42 ) )", program);
     }
 
     @Test
     public void testReplaceLists() throws Exception {
 
-        final Program parse = PARSER.parse("1 (3 (4 nil) 5 ) 6 7");
+        final Program program = PARSER.parse("1 (3 (4 nil) 5 ) 6 7");
 
         final InspectionRule replaceRule = new InspectionRule() {
             @Override
-            public void inspect(ListStruct list) {
-
-            }
+            public void inspect(ListStruct list) {}
 
             @Override
             public boolean followSublist(ListStruct list) {
@@ -71,17 +62,15 @@ public class InspectorTest {
                     list.setCar("oO");
                     return false;
                 }
-
                 return true;
             }
-
         };
 
-        LOGGER.debug("{}", FormatHelper.formatPretty(parse));
-        Inspector.inspect(parse, replaceRule);
-        LOGGER.debug("{}", FormatHelper.formatPretty(parse));
+        LOGGER.debug("{}", FormatHelper.formatPretty(program));
+        Inspector.inspect(program, replaceRule);
+        LOGGER.debug("{}", FormatHelper.formatPretty(program));
 
-        assertASTEquals("( 1 oO 6 7 )", parse);
+        assertASTEquals("( 1 oO 6 7 )", program);
     }
 
     public void assertASTEquals(String expected, Program parse) {
